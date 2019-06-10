3572e10d24643f9bb7308f67acc27114282c0131
Rasmus Porsager <rasmus@porsager.com>
2019-06-10 19:20:01 +0200

    v4.0.3

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
-  (global.PersistentWebSocket = factory());
-}(this, (function () { 'use strict';
+  (global = global || self, global.PersistentWebSocket = factory());
+}(this, function () { 'use strict';
 
   function index(url, protocols, WebSocket, options) {
     if (typeof protocols === 'function') {
@@ -245,4 +245,4 @@
 
   return index;
 
-})));
+}));
