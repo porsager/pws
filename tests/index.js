696438addb50c633f0b63a38c591d0abb3a9f0ba
Rasmus Porsager <rasmus@porsager.com>
2020-03-02 22:07:44 +0100

    Beginning of tests

---
+++ tests/index.js
@@ -0,0 +1,66 @@
+const Pws = require('../dist/index.js')
+    , ws = require('ws')
+    , { t, ot } = require('./test')
+
+function s(fn) {
+  return function(name) {
+    let socket
+      , server
+
+    return new Promise((resolve, reject) => {
+      server = new ws.Server({ port: 0 })
+      server.on('connection', socket => server.s = socket)
+      server.on('error', reject)
+      server.on('listening', () => {
+        socket = Pws('ws://localhost:' + server.address().port, ws)
+        socket.server = server
+        fn.length === 2
+          ? fn(socket, () => resolve([true, true]))
+          : Promise.resolve(fn(socket)).then(resolve)
+      })
+    }).then(async(x) => {
+      socket.close()
+      await new Promise(r => {
+        server.on('close', r)
+        server.close(r)
+      })
+      return x
+    })
+  }
+}
+
+t('onopen', s((s, done) => s.onopen = done))
+t('on(open', s((s, done) => s.on('open', done)))
+t('addEventListener open', s((s, done) => s.addEventListener('open', done)))
+
+t('onmessage', s((s, done) => (s.onmessage = done, s.onopen = () => s.server.s.send('Hi'))))
+t('on(message', s((s, done) => (s.on('message', done), s.onopen = () => s.server.s.send('Hi'))))
+t('addEventListener message', s((s, done) => (
+  s.addEventListener('message', done),
+  s.onopen = () => s.server.s.send('Hi'))
+))
+
+t('onclose', s((s, done) => (
+  s.onclose = done,
+  s.onopen = () => s.server.s.close()
+)))
+t('on(close', s((s, done) => (
+  s.on('close', done),
+  s.onopen = () => s.server.s.close()
+)))
+t('addEventListener close', s((s, done) => (
+  s.addEventListener('close', done),
+  s.onopen = () => s.server.s.close()
+)))
+
+t('reconnects', s(async(s) => {
+  let count = 0
+  await new Promise(resolve => {
+    s.onopen = () => {
+      ++count === 3
+        ? resolve()
+        : s.server.s.close()
+    }
+  })
+  return [3, 3]
+}))
